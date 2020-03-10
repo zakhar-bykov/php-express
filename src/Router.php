b82e507d3729c5dce761260c0123dd67532478e5
Zakhar Bykov <zak.bykov@gmail.com>
2020-03-10 20:48:26 +0200

    Create easy Router

--- src/Router.php
+++ src/Router.php
@@ -2,6 +2,8 @@
 
 namespace Express;
 
+require( __DIR__ . '/function/get_uri_type.php' );
+
 
 class Router {
     protected $route_rules;   // $router -> get( '/home', function( $req, $res ) => {} )
@@ -19,6 +21,103 @@ class Router {
         $this -> route_rules = $rules;
         $this -> use_rules   = $rules;
         $this -> set_rules   = $rules;
+    }
+
+
+    /**
+     * Accepts a rule for 'ALL' requests.
+     *
+     * @param  string $uri Request example as a string or regular expression.
+     * @param  array|function $options_or_callback
+     * Example = [
+     *   'http-only' => false,
+     *   'https-only' => true,
+     *   'redirect_to_another_protocol' => true
+     * ];
+     * @param  function $callback Two arguments are passed
+     * ( $request, $response ).
+     * @return void
+     */
+    public function all( $uri, $options_or_callback, ...$callback ) {
+        $this -> set_route_rule( 'ALL', $uri, $options_or_callback, $callback );
+    }
+
+
+    /**
+     * Accepts a rule for 'GET' requests.
+     *
+     * @param  string $uri Request example as a string or regular expression.
+     * @param  array|function $options_or_callback
+     * Example = [
+     *   'http-only' => false,
+     *   'https-only' => true,
+     *   'redirect_to_another_protocol' => true
+     * ];
+     * @param  function $callback Two arguments are passed
+     * ( $request, $response ).
+     * @return void
+     */
+    public function get( $uri, $options_or_callback, ...$callback ) {
+        $this -> set_route_rule( 'GET', $uri, $options_or_callback, $callback );
+    }
+
+
+    /**
+     * Accepts a rule for 'POST' requests.
+     *
+     * @param  string $uri Request example as a string or regular expression.
+     * @param  array|function $options_or_callback
+     * Example = [
+     *   'http-only' => false,
+     *   'https-only' => true,
+     *   'redirect_to_another_protocol' => true
+     * ];
+     * @param  function $callback Two arguments are passed
+     * ( $request, $response ).
+     * @return void
+     */
+    public function post( $uri, $options_or_callback, ...$callback ) {
+        $this -> set_route_rule( 'POST', $uri, $options_or_callback, $callback );
+    }
+
+
+    /**
+     * Set rules for requests.
+     *
+     * @param  string $method = ALL || GET || POST.
+     * @param  string $uri Request example as a string or regular expression.
+     * @param  array|function $options_or_callback
+     * Example = [
+     *   'http-only' => false,
+     *   'https-only' => true,
+     *   'redirect_to_another_protocol' => true
+     * ];
+     * @param  function $callback Two arguments are passed
+     * ( $request, $response ).
+     * @return void
+     */
+    protected function set_route_rule( $method, $uri, $options_or_callback, ...$callback ) {
+        $rule = [
+            'uri' => [
+                'request' => '',
+                'type' => 'string',
+            ],
+            'method' => $method,
+            'options' => [],
+            'callback' => function() {},
+        ];
+
+        $rule['uri']['request'] = $uri;
+        $rule['uri']['type'] = get_uri_type( $uri );
+
+        if ( is_callable( $options_or_callback ) ) {
+            $rule['callback'] = $options_or_callback;
+        } else if ( is_callable( $callback ) ) {
+            $rule['callback'] = $callback;
+        }
+
+        array_push( $this -> route_rules[ $method ], $rule );
+        array_push( $this -> route_rules[ 'priority' ], $rule );
     }
 }
 
